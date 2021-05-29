deca2df9c23a0192b28498edccd577c884e4ebbe
k-kumar-01 <kushalkumargupta4@gmail.com>
2021-05-29 19:46:44 +0530

    feat: adds header
    Basic header with logo and link to homepage
    
    Signed-off-by: k-kumar-01 <kushalkumargupta4@gmail.com>

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Link } from "gatsby";
+import { Container } from "@/components/common";
 
 import * as headerStyles from "./styles/header.module.scss";
 
 const Header: React.FC = (): React.ReactElement => {
-  return <header className={headerStyles.header}>BasicUI</header>;
+  return (
+    <header className={headerStyles.header}>
+      <Container>
+        <Link to="/" className={headerStyles.logo}>
+          BasicUI
+        </Link>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
