d8caad6ab7af4826d01c7b0aaa90c604df2cef5f
k-kumar-01 <kushalkumargupta4@gmail.com>
2021-06-01 14:01:17 +0530

    feat: adds features to button component
    Size property: xs,sm,md,lg,xl
    Variant property: outlined, fill
    Outlined proerty hover changes
    
    Signed-off-by: k-kumar-01 <kushalkumargupta4@gmail.com>

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -7,16 +7,20 @@ type ButtonProps = {
   className?: string;
   children?: React.ReactNode;
   type?: "basic" | "primary" | "secondary";
+  size?: "xs" | "sm" | "md" | "lg" | "xl";
+  variant?: "outlined" | "fill";
 };
 
 const Button: React.FC<ButtonProps> = ({
   className,
   text = "I am a button",
   type = "basic",
+  size = "sm",
+  variant = "outlined",
 }): React.ReactElement => {
   return (
     <button
-      className={`${ButtonStyles.button} ${ButtonStyles[type]} ${className}`}
+      className={`${ButtonStyles.button} ${ButtonStyles[type]} ${ButtonStyles[variant]} ${ButtonStyles[size]} ${className}`}
     >
       {text}
     </button>
