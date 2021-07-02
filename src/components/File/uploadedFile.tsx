00f5300c4f489cea33b905314b9cea5d1de1efe6
K-Kumar-01 <kushalkumargupta4@gmail.com>
2021-07-02 19:34:57 +0530

    feat(file): render selected Files
    ValidateFiles return {file,errors}[]
    Display file name, size and status(accpeted/errors)
    Styles update
    
    Signed-off-by: K-Kumar-01 <kushalkumargupta4@gmail.com>

---
+++ src/components/File/uploadedFile.tsx
@@ -0,0 +1,36 @@
+import React, { HTMLAttributes } from "react";
+
+import * as UploadedFileStyles from "@/styles/file/uploadedFile.module.scss";
+
+interface UploadedFileProps extends HTMLAttributes<HTMLDivElement> {
+  errors?: string;
+  name: string;
+  size: string;
+}
+
+const UploadedFile: React.FC<UploadedFileProps> = ({
+  className,
+  errors = "",
+  name,
+  size,
+}): React.ReactElement => {
+  return (
+    <div
+      className={`${UploadedFileStyles.container}${className ? className : ""}`}
+    >
+      <div className={UploadedFileStyles.fileName} title={name}>
+        {name}
+      </div>
+      <div className={UploadedFileStyles.fileSize}>({size})</div>
+      {!errors ? (
+        <div className={UploadedFileStyles.success}>Acceptable</div>
+      ) : (
+        <div className={UploadedFileStyles.error}>
+          File not supported: {errors}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UploadedFile;
