6422e746412f6d30057e26f7e6f79d77b7a82f7a
K-Kumar-01 <kushalkumargupta4@gmail.com>
2021-07-08 02:52:50 +0530

    feat(container): add padding props
    Signed-off-by: K-Kumar-01 <kushalkumargupta4@gmail.com>

--- src/components/common/Container/index.tsx
+++ src/components/common/Container/index.tsx
@@ -3,19 +3,23 @@ import React, { HTMLAttributes } from "react";
 import * as ContainerStyles from "@/styles/common/container.module.scss";
 
 interface ContainerProps extends HTMLAttributes<HTMLDivElement> {
-  children: React.ReactNode;
-  className?: string;
+  withPaddingTop?: boolean;
+  withPaddingBottom?: boolean;
 }
 
 const Container: React.FC<ContainerProps> = ({
   children,
   className,
+  withPaddingTop = false,
+  withPaddingBottom = false,
   ...props
 }): React.ReactElement => {
   return (
     <div
       {...props}
-      className={`${ContainerStyles.container} ${className ? className : ""}`}
+      className={`${ContainerStyles.container} ${className ? className : ""} ${
+        withPaddingTop ? ContainerStyles.paddingTop : ""
+      } ${withPaddingBottom ? ContainerStyles.paddingBottom : ""}`}
     >
       {children}
     </div>
